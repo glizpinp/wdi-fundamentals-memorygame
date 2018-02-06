1749c5a2c2e363625fe95fbb575342fc3c7afca2
Gliza Pinpin <glizpinp@gmail.com>
2018-02-06 01:31:08 -0800

    created game logic

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 var cards = ["queen" , "queen" , "king" , "king"];
 var cardsInPlay = [];
-var checkForMatch = function () {
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
 }
-};
-var flipCard = function (cardId) {
-cardsInPlay.push(cards[cardId]);
-console.log("User flipped " + cards[cardId]);
-checkForMatch();
-};
-flipCard(0);
-flipCard(2);
